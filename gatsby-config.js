deb96211850cc0621cf57793e3a8cc4eca2303ae
weiyang0 <nwy290@gmail.com>
2022-04-20 12:20:42 +0800

    Add mdx plugins and files to render on blog page

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,14 +4,34 @@ module.exports = {
     title: "My First Gatsby Site",
   },
   plugins: [
-    "gatsby-plugin-gatsby-cloud",
-    "gatsby-plugin-image",
-    "gatsby-plugin-sharp",
+    `gatsby-plugin-gatsby-cloud`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: `gatsby-source-filesystem`,
       options: {
         name: `blog`,
         path: `${__dirname}/blog`,
+      },
+    },
+    `gatsby-plugin-mdx`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 5000,
+              withWebp: true,
+              showCaptions: true,
+              quality: 100,
+            },
+          },
+        ],
       },
     },
   ],
