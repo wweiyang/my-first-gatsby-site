e8e757ca2ef3b30334500aed3b98e21103f4000f
weiyang0 <nwy290@gmail.com>
2022-04-27 11:18:14 +0800

    Create pages dynamically via File System Route API

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,23 +1,19 @@
 import * as React from "react";
-import { graphql } from "gatsby";
+import { Link, graphql } from "gatsby";
 import { MDXRenderer } from "gatsby-plugin-mdx";
-import Layout from "../components/layout";
+import Layout from "../../components/layout";
 
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
       {data.allMdx.nodes.map((node) => (
         <article key={node.id}>
-          <h2>{node.frontmatter.title}</h2>
+          <h2>
+            <Link to={`/blog/${node.slug}`}>{node.frontmatter.title}</Link>
+          </h2>
           <p>Posted: {node.frontmatter.date}</p>
-          <MDXRenderer>{node.body}</MDXRenderer>
         </article>
       ))}
-      {/* <ul>
-        {data.allFile.nodes.map((node) => (
-          <li key={node.name}>{node.name}</li>
-        ))}
-      </ul> */}
     </Layout>
   );
 };
@@ -31,20 +27,10 @@ export const query = graphql`
           title
         }
         id
-        body
+        slug
       }
     }
   }
 `;
-
-// export const query = graphql`
-//   query {
-//     allFile(filter: { sourceInstanceName: { eq: "blog" } }) {
-//       nodes {
-//         name
-//       }
-//     }
-//   }
-// `;
 
 export default BlogPage;
